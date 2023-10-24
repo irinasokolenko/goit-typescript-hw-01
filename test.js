c870cfeb8c04f056e1b01993ee9aa03eaf6b321b
irinasokolenko <127216070+irinasokolenko@users.noreply.github.com>
2023-10-24 15:55:34 +0300

    hw01

--- test.js
+++
@@ -1,4 +0,0 @@
-function number(num1, num2) {
-    return num1 + num2;
-}
-number(1, 1);
--- test.ts
+++
@@ -1,6 +0,0 @@
-function number(num1: number, num2: number) {
-    return num1 + num2;
-  }
-  
-  number(1, 1);
-  
